f7aae8c30da8dbb970f58d27f4bb8c3d91c11290
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-04-24 15:48:05 +0000

    ADD - added templating and database models

--- user_notes/models.py
+++ user_notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+  title = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.title
+
+class Note(models.Model):
+  title = models.CharField(max_length=200, default="New note")
+  contents = models.CharField(max_length=2000)
+  date = models.DateField()
+  time = models.TimeField()
+  subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+  def __str__(self):
+    return self.title
