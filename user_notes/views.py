fa68aafb303a49e0d0a10c051f38ebf332a48b47
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-04-25 17:38:46 +0000

    ADD - Started creating view functions and passing data from view to template

--- user_notes/views.py
+++ user_notes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Subject, Note
+# from django.urls import url
 
 # Create your views here.
 def index(request):
-  return render(request, "user_notes/index.html")
+  urls = []
+  subjects = Subject.objects.all()
+  for subject in subjects:
+    urls.append(request.build_absolute_uri() + subject.title)
+  
+  return render(request, "user_notes/index.html", {"urls":urls})
+
+def subject_notes(request, subject):
+  pass
