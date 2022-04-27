37f08212d320af0196fcb341f624fc2081b6a0ca
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-04-27 15:33:00 +0000

    ADD - Added more CSS and HTML

--- user_notes/views.py
+++ user_notes/views.py
@@ -7,9 +7,11 @@ def index(request):
   urls = []
   subjects = Subject.objects.all()
   for subject in subjects:
-    urls.append(request.build_absolute_uri() + subject.title)
+    urls.append(request.build_absolute_uri() + f"subject/{subject.title}")  
   
-  return render(request, "user_notes/index.html", {"urls":urls})
+  return render(request, "user_notes/index.html", {"urls":urls, "subjects":subjects, "iteration":range(0,len(urls))})
 
 def subject_notes(request, subject):
-  pass
+  subject_ = Subject.objects.filter(title=subject)[0]
+  notes = Note.objects.all().filter(subject=subject_.id)
+  return render(request, "user_notes/notes.html", {"notes":notes})
